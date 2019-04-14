f12449aceb1b39d612ed227e36e7899cc6b1a630
Andrea <andreabonola@gmail.com>
2019-04-13 22:10:48 -0400

    Added two components and modified css.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Posts from './components/Posts';
+import PostForm from './components/Postform'
 import './App.css';
 
 class App extends Component {
@@ -8,17 +10,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <PostForm />
+            <Posts />
         </header>
       </div>
     );
