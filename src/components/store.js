04553203fc21608b5d4e517690d4ce731f9db066
Andrea <andreabonola@gmail.com>
2019-04-14 21:06:00 -0400

    Added reducers and actions.

--- src/components/store.js
+++ src/components/store.js
@@ -1,1 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'; //no need to specify since it's in index.js
+
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+
+export default store;
