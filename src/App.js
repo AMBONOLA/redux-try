e422be3d95b4c68e47e973875b076258418b65cf
Andrea <andreabonola@gmail.com>
2019-04-30 21:06:01 -0400

    Changed a few things. Implemented redux devtools.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Posts from './components/Posts';
 import PostForm from './components/Postform'
 import './App.css';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './components/store';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <PostForm />
-            <Posts />
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+              <PostForm />
+              <Posts />
+          </header>
+        </div>
       </Provider>
     );
   }
